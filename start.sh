fb13b90387eea66e5fda2c9decea550c1f0115db
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-11-03 12:02:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/Dkmovie/video_stream_Botv2 /video_stream_Botv2
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/Dkmovie/video_stream_Botv2 -b $BRANCH /video_stream_Botv2
 fi
-cd /VCPlayerBot
+cd /video_stream_Botv2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
